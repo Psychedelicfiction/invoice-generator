0bd09a666fd0d39e1d672c23b14cc978b88ed84a
Psychedelic Fiction <mayorwa03@gmail.com>
2025-07-07 01:45:13 +0100

    packages fixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+  
+  },
+  // Optional if proxying
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://https://invoice-backend-g5q0.onrender.com/api/:path*',
+      },
+    ]
+  },
+}
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
